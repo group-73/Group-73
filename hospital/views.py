be91a23de208d0cd308d3509d1cfe8aabbeae101
Prasad <malwadkarprasad99@gmail.com>
2021-06-04 15:13:56 +0530

    lab report partial 1

--- hospital/views.py
+++ hospital/views.py
@@ -606,7 +606,7 @@ def doc_to_assdoc_view(request):
     doctor=models.Doctor.objects.get(user_id=request.user.id) #for profile picture of patient in sidebar
     mydict={'messageForm':messageForm}
     if request.method=='POST':
-        messageForm=forms.DoctortoassistantmessageForm(request.POST)
+        messageForm=forms.DoctortoassistantmessageForm(request.POST,request.FILES)
         if messageForm.is_valid():
             message=messageForm.save(commit=False)
             message.assdoctorId=request.POST.get('assdoctorId')
@@ -771,22 +771,19 @@ def  assdoctor_patient_view(request):
 @user_passes_test(is_assDoctor)
 def assdoc_to_doctor_view(request):
     messageForm=forms.AssistanttodoctormessageForm()
+
     #patient=models.Patient.objects.get(user_id=request.user.id) #for profile picture of patient in sidebar
     mydict={'messageForm':messageForm}
     if request.method=='POST':
-        messageForm=forms.AssistanttodoctormessageForm(request.POST)
+        messageForm=forms.AssistanttodoctormessageForm(request.POST,request.FILES)
         if messageForm.is_valid():
-            message=messageForm.save(commit=False)
+            message=messageForm.save()
             message.doctorId=request.POST.get('doctorId')
-            print(message.doctorId)
-            #message.assdoctorId=request.POST.get('assdoctorId')
             message.patientId=request.user.id #----user can choose any patient but only their info will be stored
-           # message.doc_name=models.Doctor.objects.get(id=request.POST.get('doctorId')).first_name
             message.assdoc_name=request.user.username
-           # message.lab_report=
-            message.Patient_name=models.User.objects.get(id=request.POST.get('patientId')).first_name #----user can choose any patient but only their info will be stored
-            message.status=True
-            message.save()
+            message.lab_report=request.POST.get('lab_report')
+            message.Patient_name=models.User.objects.get(id=request.POST.get('patientId')).first_name #----user can choose any patient but only their info will be store
+            message=message.save()
         return HttpResponseRedirect('assdoc-to-doctor')
     return render(request,'assdoc_to_doctor.html',context=mydict)
 
