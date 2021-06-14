804576a712d7c31039083bf66bd0091d6cfda94e
Prasad <malwadkarprasad99@gmail.com>
2021-06-14 11:55:40 +0530

    Project Review Done :) :) :+1:

--- hospital/views.py
+++ hospital/views.py
@@ -735,7 +735,7 @@ def assdoctor_dashboard_view(request):
 @login_required(login_url='assdoctorlogin')
 @user_passes_test(is_assDoctor)
 def assdoctor_view_patient_view(request):
-    patients=models.Patient.objects.all().filter(status=True,assdoctorId=request.user.id)
+    patients=models.Patient.objects.all().filter(status=True,assignedassDoctorId=request.user.id)
     assdoctor=models.assDoctor.objects.get(user_id=request.user.id) #for profile picture of doctor in sidebar
     return render(request,'assdoctor_view_patient.html',{'patients':patients,'doctor':assdoctor})
 
